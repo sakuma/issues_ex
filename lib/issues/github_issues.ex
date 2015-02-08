2a4afb456bf75a96402472104cc18c20f6bce667
Naohiro Sakuma <nao.bear@gmail.com>
2015-02-09 03:47:55 +0900

    extract const

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,12 +1,13 @@
 defmodule Issues.GithubIssues do
   @user_agent [ {"User-agent", "Elixir nao"} ]
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project) |> HTTPoison.get(@user_agent) |> handle_response
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response(%{status_code: 200, body: body}), do: { :ok, :jsx.decode(body) }
