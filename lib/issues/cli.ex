f70f8174f0b0941c6de52e21dbb9259c7ff91ae0
Naohiro Sakuma <nao.bear@gmail.com>
2015-02-09 03:59:42 +0900

    impl sorting

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -41,6 +41,7 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
       |> decode_response
       |> convert_to_list_of_hashdicts
+      |> sort_into_ascending_order
   end
 
   def decode_response({:ok, body}), do: body
@@ -52,6 +53,10 @@ defmodule Issues.CLI do
 
   def convert_to_list_of_hashdicts(list) do
     list |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 
 end
